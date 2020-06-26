d7b3454b3d65a70f36aa609df4bb00c20ac2b5b9
Dtown <dannyotown@gmail.com>
2020-06-25 20:32:05 -0500

    LRU done

--- src/lru_cache/lru_cache.py
+++ src/lru_cache/lru_cache.py
@@ -1,14 +1,176 @@
+
+"""Each ListNode holds a reference to its previous node
+as well as its next node in the List."""
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    def __str__(self):
+        return f'{self.value}'
+    """Wrap the given value in a ListNode and insert it
+    after this node. Note that this node could already
+    have a next node it is point to."""
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
+
+    """Wrap the given value in a ListNode and insert it
+    before this node. Note that this node could already
+    have a previous node it is point to."""
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
+
+    """Rearranges this ListNode's previous and next pointers
+    accordingly, effectively deleting this ListNode."""
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+
+
+"""Our doubly-linked list class. It holds references to
+the list's head and tail nodes."""
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+
+    def __len__(self):
+        return self.length
+    def __str__(self):
+        return f'Head:{self.head}, Tail:{self.tail}, length: {self.length}'
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new head of the list. Don't forget to handle 
+    the old head node's previous pointer accordingly."""
+    def add_to_head(self, value):
+        new_node = ListNode(value)
+        self.length += 1
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        if self.head is not None:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+    """Removes the List's current head node, making the
+    current head's next node the new head of the List.
+    Returns the value of the removed Node."""
+    def remove_from_head(self):
+        if self.head is None and self.tail is None:
+            return None
+        head_value = self.head.value
+        self.delete(self.head)
+        return head_value
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new tail of the list. Don't forget to handle 
+    the old tail node's next pointer accordingly."""
+    def add_to_tail(self, value):
+        new_node = ListNode(value)
+        self.length += 1
+        if self.head is None and self.tail is None:
+            self.tail = new_node
+            self.head = new_node
+        else:
+            old_node = self.tail
+            self.tail = new_node
+            old_node.next = new_node
+            new_node.prev = old_node
+
+
+    """Removes the List's current tail node, making the 
+    current tail's previous node the new tail of the List.
+    Returns the value of the removed Node."""
+    def remove_from_tail(self):
+        if self.head is None and self.tail is None:
+            return None
+        tail_value = self.tail.value
+        self.delete(self.tail)
+        return tail_value
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new head node of the List."""
+    def move_to_front(self, node):
+        if node is self.head:
+            return None
+        self.delete(node)
+        self.add_to_head(node.value)
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new tail node of the List."""
+    def move_to_end(self, node):
+        if node is self.tail:
+            return None
+        self.delete(node)
+        self.add_to_tail(node.value)
+
+    """Removes a node from the list and handles cases where
+    the node was the head or the tail"""
+    def delete(self, node):
+        self.length -= 1
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        elif node is self.head:
+            self.head = self.head.next
+            node.delete()
+        elif node is self.tail:
+            self.tail = self.tail.prev
+            node.delete()
+        else:
+            node.delete()
+        
+    """Returns the highest value currently in the list"""
+    def get_max(self):
+        if not self.head:
+            return None
+        max_value = self.head.value
+        current_node = self.head
+        while current_node:
+            if current_node.value > max_value:
+                max_value = current_node.value
+            current_node = current_node.next
+        return max_value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
     can hold, the current number of nodes it is holding, a doubly-
     linked list that holds the key-value entries in the correct
-    order, as well as a storage dict that provides fast access
+    order, as well as a hash_map dict that provides fast access
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.hash_map = {}
 
+        # No explicit doubly linked queue here (you may create one yourself)
+        self.list = DoublyLinkedList()
+
+        self.capacity = limit
     """
     Retrieves the value associated with the given key. Also
     needs to move the key-value pair to the end of the order
@@ -17,8 +179,13 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
-
+        if key not in self.hash_map:
+            return None
+        else:
+            # Moves it to the end if it is there
+            node = self.hash_map[key]
+            self.list.move_to_end(node)
+            return node.value[1]
     """
     Adds the given key-value pair to the cache. The newly-
     added pair should be considered the most-recently used
@@ -30,4 +197,24 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.hash_map:
+            node = self.hash_map[key]
+            node.value = (key, value)
+            self.list.move_to_end(node)
+        else:
+            if len(self.list) == self.capacity:
+                index_of_oldest = self.list.head.value[0]
+                del self.hash_map[index_of_oldest]
+                self.list.remove_from_head()
+            self.list.add_to_tail((key, value))
+            self.hash_map[key] = self.list.tail
+            
+
+
+cache = LRUCache(3)
+cache.set('item1', 'a')
+cache.set('item2', 'b')
+cache.set('item3', 'c')
+cache.set('item2', 'z')
+print(cache.hash_map)
+print(cache.list)
